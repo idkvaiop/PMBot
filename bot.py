4002948228f8b3a5881aa67054aa1a8857d273d3
BlazeOp <94062864+piroXpower@users.noreply.github.com>
2022-04-13 03:43:29 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,27 +1,28 @@
 #-------------------------------------- https://github.com/m4mallu/PMChatbot ------------------------------------------#
 import os
-from Plugins.Creator import LOGGER
+from Plugins import LOGS, BOT
 
 from pyrogram import Client
-
-logging.basicConfig(level=logging.DEBUG,
-                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-logger = logging.getLogger(__name__)
-
-logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
 if bool(os.environ.get("ENV", False)):
     from sample_config import Config
 else:
     from config import Config
 
-if __name__ == "__main__":
-    plugins = dict(root="plugins")
-    bot = Client(
-        "pmbot",
-        bot_token=Config.TG_BOT_TOKEN,
-        api_id=Config.APP_ID,
-        api_hash=Config.API_HASH,
-        plugins=plugins
-    )
-    bot.run()
+# Logging 
+basicConfig(
+    format="> %(asctime)s | %(name)s [%(levelname)s] <> %(message)s",
+    level=INFO,
+    datefmt="%d/%m/%Y, %H:%M:%S",
+    handlers=[FileHandler("PMBot.log"), StreamHandler()],
+)
+logging.getLogger("pyrogram").setLevel(logging.WARNING)
+logging.getLogger("pyrogram.dispatcher").setLevel(logging.INFO)
+LOGS.info("Starting Deployment...")
+LOGS.info("This Bot Is Powered By @TheeDeCode || @OfficialDeCode,For Any Query Contact Us")
+
+BOT.start()
+idle()
+
+BOT.stop()
+LOGS.info("Bot Stopped, Something Went Wrong..")
