4fe880bf3053c444b2cd622858ddc30a521358dd
BlazeOp <94062864+piroXpower@users.noreply.github.com>
2022-04-13 03:32:12 +0530

    Update presets.py

--- presets.py
+++ presets.py
@@ -4,4 +4,4 @@ class Presets(object):
     USER_DETAILS = "<b>PM FROM:</b>\nName: {} {}\nId: {}\nUname: @{}\nScam: {}\nRestricted: {}\nStatus: {}\nDc Id: {}"
     PM_TXT_ATT = "<b>Message from:</b> {}\n<b>Name:</b> {}\n\n{}"
     PM_MED_ATT = "<b>Message from:</b> {} \n<b>Name:</b> {}"
-    LOG_FILE_NAME = "Yukkilogs.txt"
+    
