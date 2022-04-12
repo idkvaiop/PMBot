bfa87ed195167b422f300e04d27997bb4bfe1766
BlazeOp <94062864+piroXpower@users.noreply.github.com>
2022-04-13 03:54:12 +0530

    Update __init__.py

--- Plugins/__init__.py
+++ Plugins/__init__.py
@@ -3,4 +3,4 @@ from sample_config import Config
 from pyrogram import Client
 
 LOGS = getLogger("PMBot")
-BOT = Client("pmbot", api_id=Config.APP_ID, api_hash=Config.API_HASH, bot_token=Config.TG_BOT_TOKEN, plugins=Plugins)
+BOT = Client("pmbot", api_id=Config.APP_ID, api_hash=Config.API_HASH, bot_token=Config.TG_BOT_TOKEN, plugins=dict(root="Plugins"))
