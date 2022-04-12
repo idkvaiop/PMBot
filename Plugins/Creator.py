3194f8eda39d93ffffb4cd73d3d67193b44561cb
BlazeOp <94062864+piroXpower@users.noreply.github.com>
2022-04-13 03:33:04 +0530

    Update Creator.py

--- Plugins/Creator.py
+++ Plugins/Creator.py
@@ -1,22 +1,4 @@
-import logging
-from logging.handlers import RotatingFileHandler
-
-from config import Config
-
-logging.basicConfig(
-    level=logging.INFO,
-    format="[%(asctime)s - %(levelname)s] - %(name)s - %(message)s",
-    datefmt="%d-%b-%y %H:%M:%S",
-    handlers=[
-        RotatingFileHandler(
-            LOG_FILE_NAME, maxBytes=5000000, backupCount=10
-        ),
-        logging.StreamHandler(),
-    ],
-)
-
-logging.getLogger("pyrogram").setLevel(logging.ERROR)
+from logging import getLogger
 
 
-def LOGGER(name: str) -> logging.Logger:
-    return logging.getLogger(name)
+LOGS = getLogger("PMBot")
